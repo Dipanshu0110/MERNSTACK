092ac880339bdfb76353bfa277b043b2429bad00
dipanshu0110 <dipanshuk55555@gmail.com>
2025-06-03 11:29:59 +0530

    add some new feature

--- day6/array.js
+++ day6/array.js
@@ -1,28 +1,30 @@
-// const student=["ram","shyam","rahul"];
-// console.log(student[0]);
-// console.log(student[1]);
-// console.log(student[2]);
+// // const student=["ram","shyam","rahul"];
+// // console.log(student[0]);
+// // console.log(student[1]);
+// // console.log(student[2]);
 
 
-// const student1=[];
-// student1[0]="rahul";
-// student1[1]="prem";
-// student1[2]="gyan"
-// console.log(student1);
+// // const student1=[];
+// // student1[0]="rahul";
+// // student1[1]="prem";
+// // student1[2]="gyan"
+// // console.log(student1);
 
 
-const arr= new Array(10,2,30,40,50);
-console.log(arr);
+// const arr= new Array(10,2,30,40,50);
+// console.log(arr);
 
-const arr1=new Array(10);
-console.log(arr1);
+// const arr1=new Array(10);
+// console.log(arr1);
 
-const arr2=[10,50];
-console.log(arr2);
-const len=arr2.length;
-console.log(len)
-for(let i=0; i<4;i++){
-    console.log(arr[i]);
-}
-const v =arr.sort((a,b)=>a-b)
-console.log(v);
+// const arr2=[10,50];
+// console.log(arr2);
+// const len=arr2.length;
+// console.log(len)
+// for(let i=0; i<4;i++){
+//     console.log(arr[i]);
+// }
+// const v =arr.sort((a,b)=>a-b)
+// console.log(v);
+
+
